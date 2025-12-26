9309921f4009915b9d9af8b3de86b27a919bb8f9
mantonySW <37080657+mantonySW@users.noreply.github.com>
2025-12-26 13:59:05 -0600

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import SpotifyCaseStudy from './components/SpotifyCaseStudy';
 import Home from './components/Home';
 import ExitIntentPopup from './components/ExitIntentPopup';
-import ScrollTracker from './components/ScrollTracker';
 
 export default function App() {
   return (
@@ -14,7 +13,6 @@ export default function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <ExitIntentPopup />
-      <ScrollTracker />
     </Router>
   );
 }
